c8aecfeeb25252e7cc75911362094627673a3ecd
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-03-17 21:35:03 +0500

    initial commit

---
+++ src/test/java/ru/julia/currencyexchange/CurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.julia.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
