2d3a658e2d8d993e8cf701e4951d4e954187c384
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-03-27 01:44:29 +0500

    feat: update repositories

--- src/main/java/ru/julia/currencyexchange/repository/CurrencyRepository.java
+++ src/main/java/ru/julia/currencyexchange/repository/CurrencyRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.julia.currencyexchange.entity.Currency;
 
+import java.util.Optional;
+
 @Repository
 public interface CurrencyRepository extends CrudRepository<Currency, String> {
+    Optional<Currency> findByCode(String code);
 }
--- src/main/java/ru/julia/currencyexchange/repository/SettingsRepository.java
+++ src/main/java/ru/julia/currencyexchange/repository/SettingsRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.julia.currencyexchange.entity.Settings;
 
+import java.util.Optional;
+
 @Repository
 public interface SettingsRepository extends CrudRepository<Settings, String> {
+    Optional<Settings> findByUserId(String userId);
 }
