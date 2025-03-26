b411fc592c801088d9185dc01e18479ac44d7103
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-03-26 13:48:36 +0500

    feat: add classic repository with crud

---
+++ src/main/java/ru/julia/currencyexchange/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package ru.julia.currencyexchange.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.julia.currencyexchange.entity.Role;
+import ru.julia.currencyexchange.entity.enums.RoleEnum;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, String> {
+    Optional<Role> findByRole(RoleEnum role);
+}
