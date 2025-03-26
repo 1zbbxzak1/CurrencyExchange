b8484d25164190dfb3383f63e9182ff27c20d617
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-03-26 13:52:09 +0500

    feat: update currency controller

--- src/main/java/ru/julia/currencyexchange/controller/CurrencyController.java
+++ src/main/java/ru/julia/currencyexchange/controller/CurrencyController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.julia.currencyexchange.dto.CurrencyConversion;
+import ru.julia.currencyexchange.entity.Currency;
+import ru.julia.currencyexchange.entity.CurrencyConversion;
 import ru.julia.currencyexchange.service.CurrencyExchangeService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/currency")
@@ -18,16 +20,36 @@ public class CurrencyController {
         this.converterService = converterService;
     }
 
+    @GetMapping
+    public List<Currency> updateCurrencyRates() {
+        return converterService.updateCurrencyRates();
+    }
+
     @GetMapping("/convert")
-    public double convertCurrency(
+    public CurrencyConversion convertCurrency(
+            @RequestParam String userId,
             @RequestParam String from,
             @RequestParam String to,
             @RequestParam double amount) {
-        return converterService.convert(from.toUpperCase(), to.toUpperCase(), amount);
+        return converterService.convert(userId, from.toUpperCase(), to.toUpperCase(), amount);
     }
 
     @GetMapping("/history")
-    public List<CurrencyConversion> getConversionHistory() {
-        return converterService.getConversionHistory();
+    public List<CurrencyConversion> getUserHistory(@RequestParam String userId) {
+        return converterService.getUserHistory(userId);
+    }
+
+    @GetMapping("/find")
+    public Optional<CurrencyConversion> findBySourceCurrencyAndTimestamp(
+            @RequestParam String sourceCurrency,
+            @RequestParam String timestamp) {
+        return converterService.findBySourceCurrencyAndTimestamp(sourceCurrency.toUpperCase(), timestamp);
+    }
+
+    @GetMapping("/conversion-range")
+    public Optional<List<CurrencyConversion>> getConversionByAmountRange(
+            @RequestParam Double minAmount,
+            @RequestParam Double maxAmount) {
+        return converterService.getConversionByAmountRange(minAmount, maxAmount);
     }
 }
