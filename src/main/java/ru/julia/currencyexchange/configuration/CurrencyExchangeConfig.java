00e344b1c3f541fbf22b18dabe1be30bb4f55c9a
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-03-29 21:44:25 +0500

    Домашнее задание №1 (#1)
    * feat: add name and version app
    
    * feat: add dto for currency xml
    
    * feat: add dto for currency converter history
    
    * feat: add currency repository
    
    * feat: add config for database
    
    * feat: add service for get currency cb
    
    * feat: add service for currency converter
    
    * feat: add currency controller
    
    * feat: add start settings
    
    * feat: add monitoring
    
    * chore: add swagger ui
    
    * chore: change from java 23 to java 21
    
    * chore: refactor config
    
    * chore: refactor repository

---
+++ src/main/java/ru/julia/currencyexchange/configuration/CurrencyExchangeConfig.java
@@ -0,0 +1,21 @@
+package ru.julia.currencyexchange.configuration;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CurrencyExchangeConfig {
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(
+                "Приложение: " + appName + System.lineSeparator() +
+                        "Версия: " + appVersion);
+    }
+}
