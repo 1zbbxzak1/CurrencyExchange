17a06220242bc2884faab83b7e148016eca01996
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-04-01 19:35:02 +0500

    feat: add REST to the repositories

--- src/main/java/ru/julia/currencyexchange/repository/RoleRepository.java
+++ src/main/java/ru/julia/currencyexchange/repository/RoleRepository.java
@@ -1,13 +1,13 @@
 package ru.julia.currencyexchange.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.julia.currencyexchange.entity.Role;
 import ru.julia.currencyexchange.entity.enums.RoleEnum;
 
 import java.util.Optional;
 
-@Repository
+@RepositoryRestResource(path = "roles")
 public interface RoleRepository extends CrudRepository<Role, String> {
     Optional<Role> findByRoleName(RoleEnum roleName);
 }
