9882afd3786c4cfb5340da343f644e655e46d82b
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-04-29 12:18:26 +0500

    feat: add UserDetails implementation

---
+++ src/main/java/ru/julia/currencyexchange/security/AppUserDetails.java
@@ -0,0 +1,34 @@
+package ru.julia.currencyexchange.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.julia.currencyexchange.entity.User;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class AppUserDetails implements UserDetails {
+    private final User user;
+
+    public AppUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(userRole -> new SimpleGrantedAuthority("ROLE_" + userRole.getRole().getRoleName()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
