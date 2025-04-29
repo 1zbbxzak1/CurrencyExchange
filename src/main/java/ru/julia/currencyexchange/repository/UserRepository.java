deefad737ee01b625fd056e7793a5cd43b92d908
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-04-29 12:19:43 +0500

    feat: add UserDetailsService implementation

--- src/main/java/ru/julia/currencyexchange/repository/UserRepository.java
+++ src/main/java/ru/julia/currencyexchange/repository/UserRepository.java
@@ -1,9 +1,19 @@
 package ru.julia.currencyexchange.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.julia.currencyexchange.entity.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
+    @Query("""
+            SELECT u FROM User u 
+            LEFT JOIN FETCH u.roles r 
+            LEFT JOIN FETCH r.role 
+            WHERE u.username = :username
+            """)
+    Optional<User> findByUsernameWithRoles(String username);
 }
