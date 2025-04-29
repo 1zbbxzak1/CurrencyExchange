99c1892b14d4a42a2405ec649d307efbf8763073
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-04-29 14:48:49 +0500

    feat: add register page

---
+++ src/main/java/ru/julia/currencyexchange/controller/AuthViewController.java
@@ -0,0 +1,12 @@
+package ru.julia.currencyexchange.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthViewController {
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
