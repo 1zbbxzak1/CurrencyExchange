ae3c61ae3147b3bb87c25878ad60af327ea1d27f
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-06-04 10:09:12 +0500

    Домашнее задание №5 (#8)
    * refactor: fix auth controller
    
    * feat: add entity for reports
    
    * refactor: repositories
    
    * feat: add report repository
    
    * feat: add report service
    
    * feat: add report api controller
    
    * feat: add report veiw controller
    
    * feat: add a button to open the last report
    
    * feat: add report page

--- src/main/java/ru/julia/currencyexchange/infrastructure/repository/jpa/ConversionRepository.java
+++ src/main/java/ru/julia/currencyexchange/infrastructure/repository/jpa/ConversionRepository.java
@@ -3,7 +3,6 @@ package ru.julia.currencyexchange.infrastructure.repository.jpa;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 import ru.julia.currencyexchange.domain.model.Currency;
 import ru.julia.currencyexchange.domain.model.CurrencyConversion;
 import ru.julia.currencyexchange.infrastructure.repository.criteria.CustomConversionRepository;
@@ -11,7 +10,6 @@ import ru.julia.currencyexchange.infrastructure.repository.criteria.CustomConver
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
 public interface ConversionRepository extends CrudRepository<CurrencyConversion, String>, CustomConversionRepository {
     // Поиск по имени и роли
     @Query("SELECT c FROM CurrencyConversion c WHERE c.sourceCurrency = :currency AND FUNCTION('DATE', c.timestamp) = :date")
--- src/main/java/ru/julia/currencyexchange/infrastructure/repository/jpa/SettingsRepository.java
+++ src/main/java/ru/julia/currencyexchange/infrastructure/repository/jpa/SettingsRepository.java
@@ -1,9 +1,7 @@
 package ru.julia.currencyexchange.infrastructure.repository.jpa;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import ru.julia.currencyexchange.domain.model.Settings;
 
-@Repository
 public interface SettingsRepository extends CrudRepository<Settings, String> {
 }
