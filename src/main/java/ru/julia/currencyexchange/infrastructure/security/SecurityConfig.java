ab7bfa7e139efc9cf06d88c8135c805b169137b3
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-06-23 21:02:38 +0500

    Liquibase (#13)
    * feat: add chatId and email user variables to .env.example for application.properties
    
    * delete: unuses dependencies in pom.xml
    
    * feat: add chatId and email to the User model
    
    * feat: add initial data using liquibase
    
    * feat: add initial data using liquibase
    
    * delete: unused web pages
    
    * refactoring main.yml
    
    * Update README.md

--- src/main/java/ru/julia/currencyexchange/infrastructure/security/SecurityConfig.java
+++ src/main/java/ru/julia/currencyexchange/infrastructure/security/SecurityConfig.java
@@ -2,19 +2,16 @@ package ru.julia.currencyexchange.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.julia.currencyexchange.application.service.AppUserDetailsService;
-import ru.julia.currencyexchange.domain.enums.RoleEnum;
 
 @Configuration
 @EnableMethodSecurity
@@ -32,16 +29,13 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(this::configureApiAccess)
-                .authorizeHttpRequests(this::configureHtmlAccess)
-                .formLogin(this::configureFormLogin)
-                .logout(this::configureLogout)
                 .exceptionHandling(exception -> exception
                         .defaultAuthenticationEntryPointFor(
                                 apiAuthEntryPoint,
                                 new AntPathRequestMatcher("/api/**")
                         ))
                 .userDetailsService(userDetailsService)
-                .addFilterBefore(new RedirectFilter(), UsernamePasswordAuthenticationFilter.class);
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
@@ -50,30 +44,14 @@ public class SecurityConfig {
         auth
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers(
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**",
+                        "/swagger-resources/**",
+                        "/webjars/**"
+                ).authenticated()
                 .requestMatchers("/api/**").authenticated();
-    }
-
-    private void configureHtmlAccess(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry auth) {
-        auth
-                .requestMatchers("/", "/auth", "/register").anonymous()
-                .requestMatchers("/static/**", "/css/**").permitAll()
-                .requestMatchers("/swagger-ui/**").hasRole(RoleEnum.ADMIN.name())
-                .anyRequest().authenticated();
-    }
-
-    private void configureFormLogin(FormLoginConfigurer<HttpSecurity> form) {
-        form
-                .loginPage("/auth")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/currency-rates/list", true)
-                .permitAll();
-    }
-
-    private void configureLogout(LogoutConfigurer<HttpSecurity> logout) {
-        logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/")
-                .permitAll();
     }
 
     @Bean
