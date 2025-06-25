e925233ad0bd57a81bf59ef0a21130b4fa4221cb
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-06-25 14:24:06 +0500

    feat: add verification code generator

---
+++ src/main/java/ru/julia/currencyexchange/application/service/emails/VerificationCodeGenerator.java
@@ -0,0 +1,21 @@
+package ru.julia.currencyexchange.application.service.emails;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+public class VerificationCodeGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int CODE_LENGTH = 6;
+    private static final SecureRandom RANDOM = new SecureRandom();
+
+    public String generateCode() {
+        StringBuilder code = new StringBuilder(CODE_LENGTH);
+
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            code.append(CHARACTERS.charAt(RANDOM.nextInt(CHARACTERS.length())));
+        }
+        return code.toString();
+    }
+}
