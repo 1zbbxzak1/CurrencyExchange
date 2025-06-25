e2ac8fb0c17faa1e34a69d1c91da2e9dc48e8640
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-06-25 14:29:07 +0500

    feat: update the registration POST method and add the verification POST method

--- src/main/java/ru/julia/currencyexchange/controller/AuthController.java
+++ src/main/java/ru/julia/currencyexchange/controller/AuthController.java
@@ -1,30 +1,43 @@
 package ru.julia.currencyexchange.controller;
 
-import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.julia.currencyexchange.application.dto.auth.RegisterRequest;
+import ru.julia.currencyexchange.application.dto.auth.VerifyRequest;
 import ru.julia.currencyexchange.application.service.AuthService;
-import ru.julia.currencyexchange.domain.model.User;
+import ru.julia.currencyexchange.application.service.UserService;
 
 @RestController
 @RequestMapping("/api/auth")
 @Validated
 public class AuthController {
-    private final AuthService authService;
 
-    public AuthController(AuthService authService) {
+    private final AuthService authService;
+    private final UserService userService;
+
+    public AuthController(AuthService authService, UserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/register")
-    public User createUserWithSettings(
-            @RequestParam @NotBlank(message = "Username is required") String username,
-            @RequestParam @NotBlank(message = "Email is required") String email,
-            @RequestParam @NotBlank(message = "Password is required") String password,
-            @RequestParam @NotBlank(message = "Preferred currency is required") String preferredCurrency) {
-        return authService.createUserWithSettings(username, email, password, preferredCurrency);
+    public ResponseEntity<String> createUserWithSettings(@RequestBody RegisterRequest request) {
+        authService.createUserWithVerificationCode(request.getEmail(), request.getPassword());
+
+        return ResponseEntity.ok("Код подтверждения отправлен на email: " + request.getEmail());
+    }
+
+    @PostMapping("/verify")
+    public ResponseEntity<String> verifyUser(@RequestBody VerifyRequest request) {
+        boolean result = userService.verifyUserCode(request.getEmail(), request.getCode());
+        if (result) {
+            return ResponseEntity.ok("Почта подтверждена!");
+        } else {
+            return ResponseEntity.badRequest().body("Неверный код");
+        }
     }
 }
