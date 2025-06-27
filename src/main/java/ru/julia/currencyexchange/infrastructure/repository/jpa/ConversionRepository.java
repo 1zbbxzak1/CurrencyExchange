c2762afe223b8be41c546d3769e237dd029e2c0c
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-06-27 10:29:46 +0500

    delete: unused repository implementation via Criteria API

--- src/main/java/ru/julia/currencyexchange/infrastructure/repository/jpa/ConversionRepository.java
+++ src/main/java/ru/julia/currencyexchange/infrastructure/repository/jpa/ConversionRepository.java
@@ -5,13 +5,19 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import ru.julia.currencyexchange.domain.model.Currency;
 import ru.julia.currencyexchange.domain.model.CurrencyConversion;
-import ru.julia.currencyexchange.infrastructure.repository.criteria.CustomConversionRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public interface ConversionRepository extends CrudRepository<CurrencyConversion, String>, CustomConversionRepository {
+public interface ConversionRepository extends CrudRepository<CurrencyConversion, String> {
     // Поиск по имени и роли
     @Query("SELECT c FROM CurrencyConversion c WHERE c.sourceCurrency = :currency AND FUNCTION('DATE', c.timestamp) = :date")
-    List<CurrencyConversion> findByCurrencyCodeAndDate(@Param("currency") Currency sourceCurrency, @Param("date") LocalDate date);
+    List<CurrencyConversion> findByCurrencyCodeAndDate(
+            @Param("currency") Currency sourceCurrency,
+            @Param("date") LocalDate date
+    );
+
+    // Поиск конвертаций по userId
+    @Query("SELECT c FROM CurrencyConversion c WHERE c.user.id = :userId")
+    List<CurrencyConversion> findConversionByUserId(@Param("userId") String userId);
 }
