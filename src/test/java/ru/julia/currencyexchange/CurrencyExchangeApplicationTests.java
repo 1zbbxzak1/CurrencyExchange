59d837e15a5d3a83823d744f390d9bae5df97387
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-06-29 23:27:45 +0500

    feat: add a basic test of the entire application

---
+++ src/test/java/ru/julia/currencyexchange/CurrencyExchangeApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.julia.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CurrencyExchangeApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
