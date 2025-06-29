fd628a8de9b2c4938e0ddffa7f0fdeac62fd1fea
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-06-30 00:51:03 +0500

    feat: add test properties

--- src/test/java/ru/julia/currencyexchange/CurrencyExchangeApplicationTests.java
+++ src/test/java/ru/julia/currencyexchange/CurrencyExchangeApplicationTests.java
@@ -2,8 +2,10 @@ package ru.julia.currencyexchange;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.julia.currencyexchange.utils.annotation.PostgresTestcontainers;
 
 @SpringBootTest
+@PostgresTestcontainers
 public class CurrencyExchangeApplicationTests {
     @Test
     public void contextLoads() {
