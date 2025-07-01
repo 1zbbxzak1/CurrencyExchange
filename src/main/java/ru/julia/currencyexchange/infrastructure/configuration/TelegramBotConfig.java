0826e1d4318f289ee2d9b2261d488699267433b5
Julia Polyakova <Iu.A.Poliakova@gmail.com>
2025-07-01 15:24:36 +0500

    refactoring

--- src/main/java/ru/julia/currencyexchange/infrastructure/configuration/TelegramBotConfig.java
+++ src/main/java/ru/julia/currencyexchange/infrastructure/configuration/TelegramBotConfig.java
@@ -3,10 +3,12 @@ package ru.julia.currencyexchange.infrastructure.configuration;
 import com.pengrad.telegrambot.TelegramBot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class TelegramBotConfig {
     @Bean
+    @Profile("!test")
     public TelegramBot telegramBot(BotConfig config) {
         return new TelegramBot(config.telegramToken());
     }
